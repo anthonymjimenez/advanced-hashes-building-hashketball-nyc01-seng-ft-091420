0e0f36fb95646d2b2fa6a3d937f459e45e872291
Anthony Jimenez <anthonyjim96@gmail.com>
2020-09-08 04:19:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,7 @@ def game_hash
      {
         player_name: "Alan Anderson",
         :number => 0,
-        :shoe => "16",
+        :shoe => 16,
         :points => 22,
         :rebounds => 12,
         :assists => 12,
@@ -20,7 +20,7 @@ def game_hash
       {
         player_name: "Reggie Evans",
         :number => 30,
-        :shoe => "14",
+        :shoe => 14,
         :points => 12,
         :rebounds => 12,
         :assists => 12,
@@ -31,7 +31,7 @@ def game_hash
       {
         player_name: "Brook Lopez",
         :number => 11,
-        :shoe => "17",
+        :shoe => 17,
         :points => 17,
         :rebounds => 19,
         :assists => 10,
@@ -42,7 +42,7 @@ def game_hash
        {
         player_name: "Mason Plumlee",
         :number => 1,
-        :shoe => "19",
+        :shoe => 19,
         :points => 26,
         :rebounds => 12,
         :assists => 6,
@@ -53,7 +53,7 @@ def game_hash
        {
         player_name: "Jason Terry",
         :number => 31,
-        :shoe => "15",
+        :shoe => 15,
         :points => 19,
         :rebounds => 2,
         :assists => 2,
@@ -69,7 +69,7 @@ def game_hash
     :players => [
       { player_name: "Jeff Adrien",
         :number => 4,
-        :shoe => "18",
+        :shoe => 18,
         :points => 10,
         :rebounds => 1,
         :assists => 1,
@@ -80,7 +80,7 @@ def game_hash
        {
        player_name: "Bismak Biyombo",
         :number => 0,
-        :shoe => "16",
+        :shoe => 16,
         :points => 12,
         :rebounds => 4,
         :assists => 7,
@@ -91,7 +91,7 @@ def game_hash
 {
         player_name: "Ben Gordon",
         :number => 8,
-        :shoe => "15",
+        :shoe => 15,
         :points => 33,
         :rebounds => 3,
         :assists => 2,
@@ -102,7 +102,7 @@ def game_hash
        {
         player_name: "Kemba Walker",
         :number => 33,
-        :shoe => "15",
+        :shoe => 15,
         :points => 6,
         :rebounds => 12,
         :assists => 12,
@@ -113,7 +113,7 @@ def game_hash
      {
         player_name: "DeSagna Diop",
         :number => 2,
-        :shoe => "14",
+        :shoe => 14,
         :points => 24,
         :rebounds => 12,
         :assists => 12,
